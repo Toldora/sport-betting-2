0cff2f821cdcce387a2f85603e532bf6457327ea
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2024-06-14 22:12:59 +0300

    email first

--- src/js/sign-up.js
+++ src/js/sign-up.js
@@ -5,7 +5,9 @@ import { setToLS } from '@/js/local-storage';
 const modalContentRef = document.querySelector('.js-app-modal-content');
 
 export const openSignUpModal = ({ isBlocked } = {}) => {
-  const markup = compileSignUpFormMarkup();
+  const markup = compileSignUpFormMarkup({
+    isEmailOnFirstPosition: true,
+  });
 
   modalContentRef.innerHTML = '';
   modalContentRef.insertAdjacentHTML('beforeend', markup);
